8cb3d69a90313e4d399dcc83c3d9e66ac7421899
Jonathan Sebastian <j.shn50@gmail.com>
2022-07-28 18:42:52 +0700

    deploy heroku

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+if(process.env.NODE_ENV !== 'production'){
+  require('dotenv').config()
+}
 const express = require('express');
 const cors = require("cors");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const router = require("./routes");
 const errorHandler = require("./middlewares/errorHandler")
 
