53ed54e6591c088e8cdac0c5495761b5f4f9cae8
Jonathan Sebastian <j.shn50@gmail.com>
2022-07-28 21:32:41 +0700

    updated

--- controllers/SmsController.js
+++ controllers/SmsController.js
@@ -1,8 +1,26 @@
+const { Sms } = require("../models/index");
+
 class SmsController {
   static async callbackSms(req, res, next) {
     try {
-      console.log(req.body);
-      res.status(200).json(req.body)
+      const { username, mobile, message, type } = req.body
+      const obj = {
+        username,
+        mobile,
+        message,
+        type
+      }
+      await Sms.create(obj);
+      res.status(201).json({message: "Success!"})
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  static async getSms(req, res, next) {
+    try {
+      const sms = await Sms.findAll();
+      res.status(200).json(sms);
     } catch (err) {
       next(err);
     }
